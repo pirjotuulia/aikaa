ccceed5de916f5900db38e5c8ae6c9b28c3505bb
pirjotuulia <pirjoleppanen@nic.fi>
2018-08-28 16:08:03 +0300

    gitignore

--- src/main/java/aika/aikaa/controllers/LoginController.java
+++ src/main/java/aika/aikaa/controllers/LoginController.java
@@ -1,17 +1,22 @@
-//package aika.aikaa.controllers;
-//
-//import aika.aikaa.userauth.UserAuth;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class LoginController {
-//    @Autowired
-//    UserAuth userAuth;
-//    @PostMapping("/auth")
-//    public void login(@RequestBody String idToken) {
+package aika.aikaa.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+    @PostMapping("/auth")
+    public void login(@RequestBody String idToken) {
 //        userAuth.check(idToken);
-//    }
-//}
+    }
+}
--- src/main/java/aika/aikaa/userauth/WebSecurityConfiguration.java
+++ src/main/java/aika/aikaa/userauth/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableOAuth2Sso
 @Configuration
@@ -11,13 +12,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
                 .antMatcher("/**")
                 .authorizeRequests()
                 .antMatchers("/", "/login.html")
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers("/logout/**");
     }
 }
